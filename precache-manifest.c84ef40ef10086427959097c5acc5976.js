bbdce3b2c9783a37900341f92f5ae3f1a837cefa
blackwellsmith <blackwellsmithiv@gmail.com>
2020-03-06 13:04:33 -0500

    Updates

---
+++ precache-manifest.c84ef40ef10086427959097c5acc5976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3acedc8e0ea6bc208ed4694f2e2e4b4e",
+    "url": "/my-portofolio/index.html"
+  },
+  {
+    "revision": "a2457cc89f14a8252671",
+    "url": "/my-portofolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b572d913a31c58869fc9",
+    "url": "/my-portofolio/static/js/2.0de99fc8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/my-portofolio/static/js/2.0de99fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2457cc89f14a8252671",
+    "url": "/my-portofolio/static/js/main.da274a99.chunk.js"
+  },
+  {
+    "revision": "ac0a13b7c2d7806ab153",
+    "url": "/my-portofolio/static/js/runtime-main.51d4a342.js"
+  },
+  {
+    "revision": "ebd05e7da82da7d5ed9b57a277d93536",
+    "url": "/my-portofolio/static/media/paperclip.ebd05e7d.jpg"
+  }
+]);
