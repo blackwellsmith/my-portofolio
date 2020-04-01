7f6ffd7d4e4a81d8ae83b93f6d2086d708d5b7f7
blackwellsmith <blackwellsmithiv@gmail.com>
2020-04-01 08:12:19 -0400

    no footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { About } from "./About"
 import { Resume } from "./Resume"
 import { Portfolio } from "./Portfolio"
 import { NoMatch } from "./NoMatch"
-import { Footer } from "./Footer"
+//import { Footer } from "./Footer"
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { Layout } from "./Layout"
 import { Navigation } from "./NavBar"
@@ -27,7 +27,7 @@ function App() {
           </Switch>
         </Router>
       </Layout>
-      <Footer />
+      
     </React.Fragment>
   );
 }
