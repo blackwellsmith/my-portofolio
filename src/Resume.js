06c37954c3c6e1b7ace0c1c7345ef66a7e8057dd
blackwellsmith <blackwellsmithiv@gmail.com>
2020-05-29 12:34:43 -0400

    pushing css for title

--- src/Resume.js
+++ src/Resume.js
@@ -7,7 +7,7 @@ export const Resume = () => {
             <br></br>
               <div><h2>Blackwell Smith</h2></div>
             <hr/>
-            <p>Send me an <a HREF="mailto:blackwellsmithiv@gmail.com">email</a> if you are interested in seeing a resume.</p>
+            <p><a HREF="mailto:blackwellsmithiv@gmail.com">Send me an email</a> if you are interested in seeing a resume.</p>
             <p></p>
         </div>
     )
